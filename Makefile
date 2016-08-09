c48bb3480b7f6e56b83fc44cdcc8754bff924293
artursg <ya.sagitov@gmail.com>
2016-08-09 14:41:32 +0300

    First commit. Basic reading and counting.

---
+++ Makefile
@@ -0,0 +1,4 @@
+bratfort:
+	gcc main.c -lc -o bratfort
+clean:
+	rm bratfort
---
+++ main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+char accumulator[10];
+
+int main(int argc, char ** argv)
+{
+  if (argc != 2)
+    {
+      printf("Bretfort law checker.\nUsage: ./bratfort [filename]\n");
+      return -1;
+    }
+  FILE * f = fopen(argv[1], "r");
+  memset(accumulator, 0, 10);
+  if (f == NULL)
+    {
+      printf("Error. File is not available.\n");
+      return -1;
+    }
+  char c = 0;
+  int total = 0;
+  while((c = fgetc(f)) != EOF)
+    {
+      printf("%d", c);
+      if (c >= '0' && c <= '9')
+	{
+	  accumulator[c - '0'] += 1;
+	  total++;
+	}
+    }
+  printf("Total digits: %d\n", total);
+  int i = 0;
+  for (i; i < 10; i++)
+    printf("%d: %d\n", i, accumulator[i]);
+  fclose(f);
+  return 0;
+}
